d6138e58a25c1e1fb891d80e667a5648fdd7bf22
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-28 10:13:57 +0530

    added explantion for cn method in utils.ts

--- projix/src/lib/utils.ts
+++ projix/src/lib/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+//here the cn method used to conditionally merge tailwind classes
