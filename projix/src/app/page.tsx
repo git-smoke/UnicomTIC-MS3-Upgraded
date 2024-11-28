aaa740556c0fbdc41523d1e99579382bcaa71e91
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-28 13:58:08 +0530

    Added ShadCN ui Components

--- projix/src/app/page.tsx
+++ projix/src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <Button variant={"destructive"}>Click Me</Button>
+      <p className="text-red-500 font-semibold ">Sankeerthan</p>
     </div>
   );
 }
