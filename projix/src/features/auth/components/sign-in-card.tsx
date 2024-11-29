383c5820968a4b5e34d9232caa20846760e06929
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-29 16:37:13 +0530

    created sign in card component

--- projix/src/features/auth/components/sign-in-card.tsx
+++ projix/src/features/auth/components/sign-in-card.tsx
@@ -1,3 +1,14 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
 export const SignInCard = () => {
-  return <div>Sign In Card !</div>;
+  return (
+    <Card className="w-full h-full md:w-[487px] border-none shadow-none">
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle className="text-2xl">Welcome Back!</CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        
+      </div>
+    </Card>
+  );
 };
