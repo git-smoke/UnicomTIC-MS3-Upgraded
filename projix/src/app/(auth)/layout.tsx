8e265fbe3cc5ca58bbdd70b99ae819d40de39d0b
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-29 20:15:06 +0530

    bugs fixed in sign in card

--- projix/src/app/(auth)/layout.tsx
+++ projix/src/app/(auth)/layout.tsx
@@ -1,24 +1,37 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
-const AuthLayout = ({ children }: AuthLayoutProps) => (
-  <div>
-    <main className="bg-neutral-100 min-h-screen">
-      <div className="mx-auto max-w-screen-2xl p-4">
-        <nav className="flex justify-between items-center">
-          <Image src="/logo.svg" width={152} height={50} alt="Logo" />
-          <Button variant="secondary">Sign Up</Button>
-        </nav>
-        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
-          {children}
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const pathname = usePathname();
+  const isSignIn = pathname === "/sign-in";
+
+  return (
+    <div>
+      <main className="bg-neutral-100 min-h-screen">
+        <div className="mx-auto max-w-screen-2xl p-4">
+          <nav className="flex justify-between items-center">
+            <Image src="/logo.svg" width={152} height={50} alt="Logo" />
+            <Button variant="secondary">
+              <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+                {isSignIn ? "Sign up" : "Login"}
+              </Link>
+            </Button>
+          </nav>
+          <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+            {children}
+          </div>
         </div>
-      </div>
-    </main>
-  </div>
-);
+      </main>
+    </div>
+  );
+};
 
 export default AuthLayout;
