a9980f04f953d1e5d5fe9916ce9481c3f1e11468
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-29 20:15:22 +0530

    signup card updated

--- projix/src/features/auth/components/sign-up-card.tsx
+++ projix/src/features/auth/components/sign-up-card.tsx
@@ -113,7 +113,7 @@ export const SignUpCard = () => {
               )}
             />
             <Button disabled={false} size="lg" className="w-full">
-              Login
+              Sign Up
             </Button>
           </form>
         </Form>
