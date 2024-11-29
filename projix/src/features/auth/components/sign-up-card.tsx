7329ec72dc2869430e0a842f83a35c52281b2b05
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-11-29 18:23:11 +0530

    preparing sign-up card

--- projix/src/features/auth/components/sign-up-card.tsx
+++ projix/src/features/auth/components/sign-up-card.tsx
@@ -3,20 +3,45 @@ import { FaGithub } from "react-icons/fa";
 
 import { DottedSperator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
-export const SignInCard = () => {
+export const SignUpCard = () => {
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
       <CardHeader className="flex items-center justify-center text-center p-7">
-        <CardTitle className="text-2xl">Welcome Back!</CardTitle>
+        <CardTitle className="text-2xl">Sign Up</CardTitle>
+        <CardDescription>
+          By signing up, you agree to our{" "}
+          <Link href="/privacy">
+            <span className="text-blue-700">Privacy Policy</span>
+          </Link>{" "}
+          and{" "}
+          <Link href="/terms">
+            <span className="text-blue-700">Terms of Service </span>
+          </Link>
+        </CardDescription>
       </CardHeader>
       <div className="px-7">
         <DottedSperator />
       </div>
       <CardContent className="p-7">
         <form action="" className="space-y-4">
+          <Input
+            required
+            type="text"
+            value={""}
+            onChange={() => {}}
+            placeholder="Enter your name"
+            disabled={false}
+          />
           <Input
             required
             type="email"
