029ffca157ceaee01ec4ab28806f533509f84d87
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-11 00:32:53 +0530

    initiated toolbard component

--- flow-write/src/app/documents/[documentId]/page.tsx
+++ flow-write/src/app/documents/[documentId]/page.tsx
@@ -1,8 +1,10 @@
 import { Editor } from "./editor";
+import Toolbar from "./toolbar";
 
 const DocumentPage = () => {
   return (
     <div className="min-h-screen bg-[#fafbfd]">
+      <Toolbar />
       <Editor />
     </div>
   );
