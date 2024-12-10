d4a06a64ca5d3732914f377ccc9544b21f46f8de
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-11 00:33:04 +0530

    buliding toolbar component

---
+++ flow-write/src/app/documents/[documentId]/toolbar.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/lib/utils";
+import { LucideIcon, Undo2Icon } from "lucide-react";
+
+interface ToolbarButtonProps {
+  onClick?: () => void;
+  isActive?: boolean;
+  icon: LucideIcon;
+}
+
+const ToolbarButton = ({
+  onClick,
+  isActive,
+  icon: Icon,
+}: ToolbarButtonProps) => {
+  <button
+    onClick={onClick}
+    className={cn(
+      "text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80",
+      isActive && "bg-neutral-200/80"
+    )}
+  >
+    <Icon className="size-4" />
+  </button>;
+};
+
+const Toolbar = () => {
+  const sections: {
+    label: string;
+    icon: LucideIcon;
+    onClick: () => void;
+    isActive?: boolean;
+  }[][] = [
+    [
+      {
+        label: "Undo",
+        icon: Undo2Icon,
+        onClick: () => console.log("Undo Clicked"),
+      },
+    ],
+  ];
+
+  return (
+    <div className="bg-[#f1f4f9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
+      Tool Bar !
+    </div>
+  );
+};
+
+export default Toolbar;
