129d299c7eeed905d66af5e55823896b7ed8202e
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-10 11:26:27 +0530

    imported task Item and List for the Extension

--- flow-write/src/app/documents/[documentId]/editor.tsx
+++ flow-write/src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,8 @@
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import { StarterKit } from "@tiptap/starter-kit";
+import { TaskItem } from "@tiptap/extension-task-item";
+import { TaskList } from "@tiptap/extension-task-list";
 
 export const Editor = () => {
   const editor = useEditor({
@@ -12,11 +14,11 @@ export const Editor = () => {
           "focus:outline-none print:border-0 bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
+    extensions: [StarterKit, TaskItem.configure({ nested: true }), TaskList],
     content: "<p>Hello World  🌎️!</p>",
   });
 
-return (
+  return (
     <div className="size-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:bg-white print:overflow-visible ">
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
