e2db75ea43207fc36de82e6d809b22d4f16f3174
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-10 11:42:48 +0530

    tested tiptap table

--- flow-write/src/app/documents/[documentId]/editor.tsx
+++ flow-write/src/app/documents/[documentId]/editor.tsx
@@ -27,7 +27,22 @@ export const Editor = () => {
       TaskItem.configure({ nested: true }),
       TaskList,
     ],
-    content: "<p>Hello World  🌎️!</p>",
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
   });
 
   return (
