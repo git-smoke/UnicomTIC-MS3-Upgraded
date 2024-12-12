a6fe2b4f534e4f705a2224528b62aedcc784ab91
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-12 16:54:00 +0530

    created highlighter button funciton with logics

--- flow-write/src/app/documents/[documentId]/toolbar.tsx
+++ flow-write/src/app/documents/[documentId]/toolbar.tsx
@@ -12,6 +12,7 @@ import { useEditorStore } from "@/store/use-editor-store";
 import {
   BoldIcon,
   ChevronDownIcon,
+  HighlighterIcon,
   ItalicIcon,
   ListTodoIcon,
   LucideIcon,
@@ -26,6 +27,31 @@ import {
 
 import { type Level } from "@tiptap/extension-heading";
 import { type ColorResult, SketchPicker } from "react-color";
+
+
+const HighLightColorButton = () => {
+  const { editor } = useEditorStore();
+
+  const value = editor?.getAttributes("highlight").color || "#ffffff"
+
+  const onChange = (color: ColorResult) => {
+    editor?.chain().focus().setHighlight({ color: color.hex }).run();
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-5 min-w-7 shrink-0 flex flex-col items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+        >
+          <HighlighterIcon className="size-4" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-0">
+        <SketchPicker onChange={onChange} color={value} />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
 
 
 const TextColorButton = () => {
@@ -325,7 +351,7 @@ const Toolbar = () => {
         <ToolbarButton key={item.label} {...item} />
       ))}
       <TextColorButton />
-      {/* TODO: Highlight color */}
+      <HighLightColorButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {/* TODO Link */}
       {/* TODO Image*/}
