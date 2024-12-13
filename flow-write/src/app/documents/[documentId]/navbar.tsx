cdcf4948c82a5d00a4a72a838f1dd17b1e1de69b
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-13 23:53:59 +0530

    updated menu items for the nav bar

--- flow-write/src/app/documents/[documentId]/navbar.tsx
+++ flow-write/src/app/documents/[documentId]/navbar.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
 import { DocumentInput } from "./document-input"
-import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar"
-import { FileIcon } from "lucide-react"
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarShortcut, MenubarSub, MenubarSubContent, MenubarSubTrigger, MenubarTrigger } from "@/components/ui/menubar"
+import { BoldIcon, FileIcon, FileJsonIcon, FilePenIcon, FilePlusIcon, FileTextIcon, GlobeIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, TextIcon, TrashIcon, Undo2Icon } from "lucide-react"
+import { BsFilePdf, BsTypeBold, BsTypeItalic, BsTypeStrikethrough, BsTypeUnderline } from "react-icons/bs"
 
 export const Navbar = () => {
     return (
@@ -16,13 +19,143 @@ export const Navbar = () => {
                     <div className="flex">
                         <Menubar className="border-none bg-transparent shadow-none h-auto p-0" >
                             <MenubarMenu>
-                                <MenubarTrigger>
+                                <MenubarTrigger
+                                    className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
                                     File
+                                </MenubarTrigger>
+                                <MenubarContent className="print:hidden">
+                                    <MenubarSub>
+                                        <MenubarSubTrigger>
+                                            <FileIcon className="size-4 mr-2" />
+                                            Save
+                                        </MenubarSubTrigger>
+                                        <MenubarSubContent>
+                                            <MenubarItem>
+                                                <FileJsonIcon className="size-4 mr-2" />
+                                                JSON
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                <GlobeIcon className="size-4 mr-2" />
+                                                HTMl
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                <BsFilePdf className="size-4 mr-2" />
+                                                PDF
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                <FileTextIcon className="size-4 mr-2" />
+                                                Text
+                                            </MenubarItem>
+                                        </MenubarSubContent>
+                                    </MenubarSub>
+                                    <MenubarItem>
+                                        <FilePlusIcon className="size-4 mr-2" />
+                                        New Document
+                                    </MenubarItem>
+                                    <MenubarSeparator />
+                                    <MenubarItem>
+                                        <FilePenIcon className="size-4 mr-2" />
+                                        Rename
+                                    </MenubarItem>
+                                    <MenubarItem>
+                                        <TrashIcon className="size-4 mr-2" />
+                                        Remove
+                                    </MenubarItem>
+                                    <MenubarSeparator />
+                                    <MenubarItem onClick={() => window.print()}>
+                                        <PrinterIcon className="size-4 mr-2" />
+                                        Print <MenubarShortcut>
+                                            ⌘P
+                                        </MenubarShortcut>
+                                    </MenubarItem>
+                                </MenubarContent>
+                            </MenubarMenu>
+                            <MenubarMenu>
+                                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                                    Edit
                                 </MenubarTrigger>
                                 <MenubarContent>
                                     <MenubarItem>
-                                        <FileIcon className="size-4 mr-2" />
-                                        Save
+                                        <Undo2Icon className="size-4 mr-2" />
+                                        Undo <MenubarShortcut>
+                                            ⌘Z
+                                        </MenubarShortcut>
+                                    </MenubarItem>
+                                    <MenubarItem>
+                                        <Redo2Icon className="size-4 mr-2" />
+                                        Redo <MenubarShortcut>
+                                            ⌘Y
+                                        </MenubarShortcut>
+                                    </MenubarItem>
+                                </MenubarContent>
+                            </MenubarMenu>
+                            <MenubarMenu>
+                                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                                    Insert
+                                </MenubarTrigger>
+                                <MenubarContent>
+                                    <MenubarSub>
+                                        <MenubarSubTrigger>
+                                            Table
+                                        </MenubarSubTrigger>
+                                        <MenubarSubContent>
+                                            <MenubarItem>
+                                                1 x 1
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                2 x 2
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                3 x 3
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                4 x 4
+                                            </MenubarItem>
+                                        </MenubarSubContent>
+                                    </MenubarSub>
+                                </MenubarContent>
+                            </MenubarMenu>
+                            <MenubarMenu>
+                                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                                    Format
+                                </MenubarTrigger>
+                                <MenubarContent>
+                                    <MenubarSub>
+                                        <MenubarSubTrigger>
+                                            <TextIcon className="size-4 mr-2" />
+                                            Text
+                                        </MenubarSubTrigger>
+                                        <MenubarSubContent>
+                                            <MenubarItem>
+                                                <BsTypeBold className="size-4 mr-2" />
+                                                Bold <MenubarShortcut>
+                                                    ⌘B
+                                                </MenubarShortcut>
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                <BsTypeItalic className="size-4 mr-2" />
+                                                Italic<MenubarShortcut>
+                                                    ⌘I
+                                                </MenubarShortcut>
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                <BsTypeUnderline className="size-4 mr-2" />
+                                                Underline&nbsp;&nbsp;&nbsp;
+                                                <MenubarShortcut>
+                                                    ⌘U
+                                                </MenubarShortcut>
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                <BsTypeStrikethrough className="size-4 mr-2" />
+                                                Strike <MenubarShortcut>
+                                                    ⌘S
+                                                </MenubarShortcut>
+                                            </MenubarItem>
+                                        </MenubarSubContent>
+                                    </MenubarSub>
+                                    <MenubarItem>
+                                        <RemoveFormattingIcon className="size-4 mr-2" />
+                                        Clear formatting
                                     </MenubarItem>
                                 </MenubarContent>
                             </MenubarMenu>
