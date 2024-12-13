4d77fee5729b8bf0113c4316aadeb4017d0644a2
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 03:36:17 +0530

    updated templates

--- flow-write/src/constants/templates.ts
+++ flow-write/src/constants/templates.ts
@@ -4,5 +4,35 @@ export const templates = [
         id: "blank",
         label: "Blank Document",
         imageUrl: "/blank-document.svg"
-    }
+    },
+    {
+        id: "business-letter",
+        label: "Business Letter",
+        imageUrl: "/business-letter.svg"
+    },
+    {
+        id: "resume",
+        label: "Resume",
+        imageUrl: "/resume.svg"
+    },
+    {
+        id: "software-proposal",
+        label: " Software development proposal",
+        imageUrl: "/software-proposal.svg"
+    },
+    {
+        id: "project-proposal",
+        label: "Project proposal",
+        imageUrl: "/project-proposal.svg"
+    },
+    {
+        id: "cover-letter",
+        label: "Cover letter",
+        imageUrl: "/cover-letter.svg"
+    },
+    {
+        id: "letter",
+        label: "Letter",
+        imageUrl: "/letter.svg"
+    },
 ]
