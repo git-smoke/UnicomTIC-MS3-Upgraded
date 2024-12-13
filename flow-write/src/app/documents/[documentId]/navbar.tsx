69ffcc87f7ccfe757f1fd8fc93016d88ce2cc74e
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-13 22:10:30 +0530

    nav bar styles updated

--- flow-write/src/app/documents/[documentId]/navbar.tsx
+++ flow-write/src/app/documents/[documentId]/navbar.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export const Navbar = () => {
     return (
-        <nav>
-            <Image src="/logo.svg" alt="Logo" />
+        <nav className="flex items-center justify-between">
+            <div className="flex gap-2 items-center">
+                <Link href="/">
+                    <Image src="/logo.svg" alt="Logo" width={36} height={36} />
+                </Link>
+                <div className="flex flex-col">
+                    {/* Document Input */}
+                    {/* Menu Bar */}
+                </div>
+            </div>
         </nav>
     )
 }
