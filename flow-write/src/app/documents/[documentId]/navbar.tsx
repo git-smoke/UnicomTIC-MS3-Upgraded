7feef6e0a14be7a9a25f0ae7d580f9fa3f5284e1
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-13 22:24:16 +0530

    basic styles in the menu bar

--- flow-write/src/app/documents/[documentId]/navbar.tsx
+++ flow-write/src/app/documents/[documentId]/navbar.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image"
 import Link from "next/link"
 import { DocumentInput } from "./document-input"
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar"
+import { FileIcon } from "lucide-react"
 
 export const Navbar = () => {
     return (
@@ -11,7 +13,21 @@ export const Navbar = () => {
                 </Link>
                 <div className="flex flex-col">
                     <DocumentInput />
-                    {/* Menu Bar */}
+                    <div className="flex">
+                        <Menubar className="border-none bg-transparent shadow-none h-auto p-0" >
+                            <MenubarMenu>
+                                <MenubarTrigger>
+                                    File
+                                </MenubarTrigger>
+                                <MenubarContent>
+                                    <MenubarItem>
+                                        <FileIcon className="size-4 mr-2" />
+                                        Save
+                                    </MenubarItem>
+                                </MenubarContent>
+                            </MenubarMenu>
+                        </Menubar>
+                    </div>
                 </div>
             </div>
         </nav>
