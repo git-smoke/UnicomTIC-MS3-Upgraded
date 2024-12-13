02cf99c7a3f8ad8a5249d04532e53b80cce30443
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 05:06:10 +0530

    bugs fixed with convex

--- flow-write/convex/_generated/api.d.ts
+++ flow-write/convex/_generated/api.d.ts
@@ -13,7 +13,6 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as documents from "../documents.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -23,9 +22,7 @@ import type * as documents from "../documents.js";
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{
-  documents: typeof documents;
-}>;
+declare const fullApi: ApiFromModules<{}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
