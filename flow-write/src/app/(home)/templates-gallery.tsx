af94db2954ebb5d5c707207f4e4014cc57d45f7d
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 03:23:59 +0530

    updated carousel for the templates

--- flow-write/src/app/(home)/templates-gallery.tsx
+++ flow-write/src/app/(home)/templates-gallery.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel"
+import { cn } from "@/lib/utils"
 
 
 const templates = [{
@@ -10,6 +11,9 @@ const templates = [{
 }]
 
 export const TemplatesGallery = () => {
+
+    const isCreating = false;
+
     return (
         <div className="bg-[#f1f3f4]">
             <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-y-4">
@@ -23,7 +27,27 @@ export const TemplatesGallery = () => {
                                 key={template.id}
                                 className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6 2xl:basis-[14.285714%] pl-4"
                             >
-                                
+                                <div
+                                    className={cn(
+                                        "aspect-[3/4] flex flex-col gap-y-2.5",
+                                        isCreating && "pointer-events-none opacity-50"
+                                    )}
+                                >
+                                    <button
+                                        disabled={isCreating}
+                                        onClick={() => { }}
+                                        style={{
+                                            backgroundImage: `url(${template.imageUrl})`,
+                                            backgroundSize: "cover",
+                                            backgroundPosition: "center",
+                                            backgroundRepeat: "no-repeat"
+                                        }}
+                                        className="size-full hover:border-blue-500 rounded-sm border hover:bg-blue-50 transition flex flex-col items-center justify-center gap-y-4 bg-white"
+                                    />
+                                    <p className="text-sm font-medium truncate">
+                                        {template.label}
+                                    </p>
+                                </div>
                             </CarouselItem>
                         ))}
                     </CarouselContent>
