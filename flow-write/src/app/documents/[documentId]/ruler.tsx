5e2dc9e2ebf5cd50d95375314aa0081c97f1f9f1
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-13 19:43:54 +0530

    ruler styles init

--- flow-write/src/app/documents/[documentId]/ruler.tsx
+++ flow-write/src/app/documents/[documentId]/ruler.tsx
@@ -1,7 +1,14 @@
+
+
 export const Ruler = () => {
     return (
-        <div>
-            Ruler 
+        <div className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
+            <div
+                id="ruler-container"
+                className="max-w-[816px] mx-auto w-full h-full relative"
+            >
+
+            </div>
         </div>
     )
 }
