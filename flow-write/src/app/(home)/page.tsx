7ca7458e3d457a553b522595fe6ca56776743cf6
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 01:49:02 +0530

    style update for home main div

--- flow-write/src/app/(home)/page.tsx
+++ flow-write/src/app/(home)/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Navbar } from "./navbar";
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+
+    <div className="min-h-screen flex flex-col">
+      <div><Navbar /></div>
       <Button variant={"destructive"}>
         <Link href="/documents/76176"> Click Me</Link>
       </Button>
