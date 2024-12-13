990749a4c3e979a5a562c4db33446ee4e759fdbe
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 05:23:52 +0530

    switched from clerk-react to nextjs

--- flow-write/src/components/convex-client-provider.tsx
+++ flow-write/src/components/convex-client-provider.tsx
@@ -3,7 +3,7 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 import { ConvexProviderWithClerk } from "convex/react-clerk"
-import { ClerkProvider, useAuth } from "@clerk/clerk-react"
+import { ClerkProvider, useAuth } from "@clerk/nextjs"
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
