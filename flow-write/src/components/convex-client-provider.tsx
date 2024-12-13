105616048e9e883b10785bc776fad70579dac813
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 05:29:32 +0530

    added extra imports

--- flow-write/src/components/convex-client-provider.tsx
+++ flow-write/src/components/convex-client-provider.tsx
@@ -1,16 +1,21 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexReactClient,AuthLoading,Authenticated,Unauthenticated } from "convex/react";
 import { ReactNode } from "react";
 import { ConvexProviderWithClerk } from "convex/react-clerk"
-import { ClerkProvider, useAuth } from "@clerk/nextjs"
+import { ClerkProvider, useAuth } from "@clerk/clerk-react"
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
   return (
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
-      <ConvexProvider client={convex}>{children}</ConvexProvider>
+      <ConvexProviderWithClerk
+        useAuth={useAuth}
+        client={convex}
+      >
+        {children}
+      </ConvexProviderWithClerk>
     </ClerkProvider>
   );
 }
