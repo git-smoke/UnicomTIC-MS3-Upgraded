f680fda49864f0a3ab0191b314e7525fb5d3752b
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-13 22:12:13 +0530

    basic update for document input

---
+++ flow-write/src/app/documents/[documentId]/document-input.tsx
@@ -0,0 +1,9 @@
+export const DocumentInput = () => {
+    return (
+        <div className="flex items-center gap-2">
+            <span className="text-lg px-1.5 cursor-pointer truncate">
+                Untitled Document
+            </span>
+        </div>
+    )
+}
