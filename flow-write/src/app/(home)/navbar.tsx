b0df9f3c33de77b69dcfa8734c8252dcd5a6c511
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 01:55:56 +0530

    basic nav styles updated

--- flow-write/src/app/(home)/navbar.tsx
+++ flow-write/src/app/(home)/navbar.tsx
@@ -4,8 +4,11 @@ import Image from "next/image"
 export const Navbar = () => {
     return (
         <nav className="flex items-center justify-between h-full w-full">
-            <div className="">
-
+            <div className="flex gap-3 items-center shrink-0 pr-6">
+                <Link href="/">
+                    <Image src="/logo.svg" alt="logo" width={36} height={36} />
+                </Link>
+                <h3 className="text-xl">FloWrite</h3>
             </div>
         </nav>
     )
