d7e79269c081bc815c3bfea921596dcc321b1452
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 05:23:19 +0530

    updated entire app with clerk provided

--- flow-write/src/components/convex-client-provider.tsx
+++ flow-write/src/components/convex-client-provider.tsx
@@ -2,9 +2,15 @@
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
+import { ConvexProviderWithClerk } from "convex/react-clerk"
+import { ClerkProvider, useAuth } from "@clerk/clerk-react"
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+      <ConvexProvider client={convex}>{children}</ConvexProvider>
+    </ClerkProvider>
+  );
 }
