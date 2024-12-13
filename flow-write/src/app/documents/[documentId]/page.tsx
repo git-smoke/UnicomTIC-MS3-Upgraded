89d0490fc83531656d6700ca34d0685daa4ca4e1
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-13 22:00:21 +0530

    imported navbar to the page in docId

--- flow-write/src/app/documents/[documentId]/page.tsx
+++ flow-write/src/app/documents/[documentId]/page.tsx
@@ -1,9 +1,11 @@
 import { Editor } from "./editor";
+import { Navbar } from "./navbar";
 import Toolbar from "./toolbar";
 
 const DocumentPage = () => {
   return (
     <div className="min-h-screen bg-[#fafbfd]">
+      <Navbar />
       <Toolbar />
       <Editor />
     </div>
