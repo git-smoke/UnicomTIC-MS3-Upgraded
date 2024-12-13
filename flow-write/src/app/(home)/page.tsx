494df1380a74ccbf421c0569c2f14e863b25ce6d
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 01:57:04 +0530

    padding update for home

--- flow-write/src/app/(home)/page.tsx
+++ flow-write/src/app/(home)/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
 
     <div className="min-h-screen flex flex-col">
-      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white">
+      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
         <Navbar />
       </div>
       <div className="mt-16">
