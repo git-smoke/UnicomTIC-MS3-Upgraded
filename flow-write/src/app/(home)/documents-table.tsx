bed5529f52805acef05910758cf2aac9643758c8
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 06:26:55 +0530

    created doc table interface and fcuntions

---
+++ flow-write/src/app/(home)/documents-table.tsx
@@ -0,0 +1,20 @@
+import { PaginationStatus } from "convex/react";
+import { Doc } from "../../../convex/_generated/dataModel";
+
+interface DocumentsTableProps {
+    documents: Doc<"documents">[] | undefined;
+    loadMore: (numItems: number) => void;
+    status: PaginationStatus
+}
+
+export const DocumentsTable = ({
+    documents,
+    loadMore,
+    status,
+}: DocumentsTableProps) => {
+    return(
+        <div>
+            Documents Table
+        </div>
+    )
+}
