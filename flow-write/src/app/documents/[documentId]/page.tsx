b222bfe027e5d623a19499b181f06313a000abdb
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 16:11:58 +0530

    page component reInit

--- flow-write/src/app/documents/[documentId]/page.tsx
+++ flow-write/src/app/documents/[documentId]/page.tsx
@@ -1,25 +1,11 @@
-import { Editor } from "./editor";
-import { Navbar } from "./navbar";
-import { Room } from "./room";
-import Toolbar from "./toolbar";
+import { Id } from "../../../../convex/_generated/dataModel";
 
-const DocumentPage = () => {
+interface DocumentIdPageProps {
+  params: Promise<{ documentId: Id<"documents"> }>;
+}
 
+const DocumentIdPage = () => {
+  return ();
+}
 
-
-  return (
-    <Room>
-      <div className="min-h-screen bg-[#fafbfd]">
-        <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#fafbfd] print:hidden">
-          <Navbar />
-          <Toolbar />
-        </div>
-        <div className="pt-[114px] print:pt-0">
-          <Editor />
-        </div>
-      </div>
-    </Room>
-  );
-};
-
-export default DocumentPage;
+export default DocumentIdPage;
