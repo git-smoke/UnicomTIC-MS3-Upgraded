1bf8912b4ac1498f95295e387c653417f07ec1de
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 12:54:23 +0530

    api endpoint init for user recognization in colabs

--- flow-write/src/app/api/liveblocks-auth/route.ts
+++ flow-write/src/app/api/liveblocks-auth/route.ts
@@ -1,9 +1,15 @@
-import { Livebloks } from "@liveblocks/node"
+
 import { ConvexHttpClient } from "convex/browser"
 import { auth, currentUser } from "@clerk/nextjs/server"
+import { getById } from '../../../../convex/documents';
+import { api } from "../../../../convex/_generated/api";
+import { Liveblocks } from "@liveblocks/node";
 
 
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
+const liveblocks = new Liveblocks({
+    secret: process.env.LIVEBLOCKS_SECRET_KEY!,
+});
 
 export async function POST(req: Request) {
     const { sessionClaims } = await auth();
@@ -17,4 +23,29 @@ export async function POST(req: Request) {
     }
 
     const { room } = await req.json();
+    const document = await convex.query(api.documents.getById, {
+        id: room
+    })
+    if (!document) {
+        return new Response("Unauthorized", { status: 401 });
+    }
+
+    const isOwner = document.ownerId === user.id;
+    const isOrganizationMember = document.organizationId === sessionClaims.org_id;
+
+    if (!isOwner && !isOrganizationMember) {
+        return new Response("Unauthorized", { status: 401 })
+    }
+
+    const session = liveblocks.prepareSession(user.id, {
+        userInfo: {
+            name: user.fullName ?? "Anonymus",
+            avatar: user.imageUrl,
+        }
+    })
+    session.allow(room, session.FULL_ACCESS)
+    const { body, status } = await session.authorize()
+
+    return new Response(body, { status })
+
 }
