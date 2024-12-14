098cd8db2bfa994a9abed92941bb80d294d3c764
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 07:29:54 +0530

    documentrow props interface init

---
+++ flow-write/src/app/(home)/document-row.tsx
@@ -0,0 +1,6 @@
+import { TableCell, TableRow } from "@/components/ui/table";
+import { Doc } from "../../../convex/_generated/dataModel";
+
+interface DocumentRowProps {
+    document: Doc<"documents">;
+}
