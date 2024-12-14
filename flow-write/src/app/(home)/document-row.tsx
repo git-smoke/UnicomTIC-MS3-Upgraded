cecb0bcbcc10c8487c9ef455fadff5f1467fd9a8
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 07:52:29 +0530

    document row update with menu import

--- flow-write/src/app/(home)/document-row.tsx
+++ flow-write/src/app/(home)/document-row.tsx
@@ -4,6 +4,7 @@ import { SiGoogledocs } from "react-icons/si";
 import { BuildingIcon, CircleUserIcon, icons, MoreVertical } from "lucide-react";
 import { format } from "date-fns";
 import { Button } from "@/components/ui/button";
+import { DocumentMenu } from "./document-menu";
 interface DocumentRowProps {
     document: Doc<"documents">;
 }
@@ -27,9 +28,7 @@ export const DocumentRow = ({ document }: DocumentRowProps) => {
                 {format(new Date(document._creationTime), "MMM dd, yyyy")}
             </TableCell>
             <TableCell className="flex justify-end ">
-                <Button variant="ghost" size="icon" className="rounded-full">
-                    <MoreVertical className="size-4" />
-                </Button>
+                <DocumentMenu />
             </TableCell>
         </TableRow>
     );
