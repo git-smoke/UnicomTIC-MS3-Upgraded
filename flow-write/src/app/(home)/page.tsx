e6138ae8bfd6fd5d7aaba492c9b83b0c5fcfb1a2
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 06:22:57 +0530

    updated paginated query

--- flow-write/src/app/(home)/page.tsx
+++ flow-write/src/app/(home)/page.tsx
@@ -1,18 +1,15 @@
 "use client";
-import { useQuery } from "convex/react";
+import { usePaginatedQuery } from "convex/react";
 import { Navbar } from "./navbar";
 import { TemplatesGallery } from "./templates-gallery";
 import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
-  const documents = useQuery(api.documents.get);
+  const {
+    results
+    , status,
+    loadMore } = usePaginatedQuery(api.documents.get, {}, { initialNumItems: 5 });
 
-
-  if (documents === undefined) {
-    return (
-      <p>Loading...</p>
-    )
-  }
 
   return (
 
@@ -22,11 +19,11 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplatesGallery />
-        {documents?.map((document) => (
-          <span key={document._id}>
-            {document.title}
-          </span>
-        ))}
+        <DocumentsTable
+          documents={results}
+          loadMore={loadMore}
+          status={status}
+        />
       </div>
     </div>
   );
