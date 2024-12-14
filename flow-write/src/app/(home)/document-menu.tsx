b38df6e3380642f5130ace57d96d2f150c7bf978
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 07:56:52 +0530

    documentMenu props interface init

--- flow-write/src/app/(home)/document-menu.tsx
+++ flow-write/src/app/(home)/document-menu.tsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button"
 import { MoreVertical } from "lucide-react"
+import { Id } from "../../../convex/_generated/dataModel"
+
+interface DocumentMenuProps {
+    documentId: Id<"documents">;
+    title: string;
+    onNewTab: (id: Id<"documents">) => void;
+}
 
 export const DocumentMenu = () => {
     return (
