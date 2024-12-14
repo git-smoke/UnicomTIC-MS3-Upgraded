79ccc0e72de38af79d33c4567b6c455d1269859e
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 22:32:01 +0530

    basic component functions init

--- flow-write/src/app/documents/[documentId]/document.tsx
+++ flow-write/src/app/documents/[documentId]/document.tsx
@@ -2,3 +2,26 @@ import { Room } from "./room"
 import { Editor } from "./editor"
 import { Navbar } from "./navbar"
 import { Toolbar } from "./toolbar"
+import { Preloaded } from "convex/react"
+import { api } from "../../../../convex/_generated/api"
+
+interface DocumentProps {
+    preloadedDocument: Preloaded<typeof api.documents.getById>
+}
+
+export const Document = ({ preloadedDocument }: DocumentProps) => {
+    return (
+        <Room>
+            <div className="min-h-screen bg-[#fafbfd]">
+                <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#fafbfd] print:hidden">
+                    <Navbar />
+                    <Toolbar />
+                </div>
+                <div className="pt-[114px] print:pt-0">
+                    <Editor />
+                </div>
+            </div>
+        </Room>
+    )
+
+}
