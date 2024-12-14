b153de7e01e44751326241f4b87da03f43969d70
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 07:48:27 +0530

    updated table

--- flow-write/src/app/(home)/document-row.tsx
+++ flow-write/src/app/(home)/document-row.tsx
@@ -1,8 +1,9 @@
 import { TableCell, TableRow } from "@/components/ui/table";
 import { Doc } from "../../../convex/_generated/dataModel";
 import { SiGoogledocs } from "react-icons/si";
-import { BuildingIcon, CircleUserIcon } from "lucide-react";
-
+import { BuildingIcon, CircleUserIcon, icons, MoreVertical } from "lucide-react";
+import { format } from "date-fns";
+import { Button } from "@/components/ui/button";
 interface DocumentRowProps {
     document: Doc<"documents">;
 }
@@ -23,7 +24,12 @@ export const DocumentRow = ({ document }: DocumentRowProps) => {
                 {document.organizationId ? "Organization" : "Personal"}
             </TableCell>
             <TableCell className="text-muted-foreground hidden md:table-cell">
-
+                {format(new Date(document._creationTime), "MMM dd, yyyy")}
+            </TableCell>
+            <TableCell className="flex justify-end ">
+                <Button variant="ghost" size="icon" className="rounded-full">
+                    <MoreVertical className="size-4" />
+                </Button>
             </TableCell>
         </TableRow>
     );
