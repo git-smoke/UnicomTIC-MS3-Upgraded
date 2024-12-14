28effaf3e780b9cf0f4c653cfa193da4bd155007
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 09:39:31 +0530

    updated pagination Ui

--- flow-write/src/app/(home)/documents-table.tsx
+++ flow-write/src/app/(home)/documents-table.tsx
@@ -3,6 +3,7 @@ import { Doc } from "../../../convex/_generated/dataModel";
 import { LoaderIcon } from "lucide-react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { DocumentRow } from "./document-row";
+import { Button } from "@/components/ui/button";
 
 interface DocumentsTableProps {
     documents: Doc<"documents">[] | undefined;
@@ -22,48 +23,55 @@ export const DocumentsTable = ({
                     <LoaderIcon className="animate-spin text-muted-foreground size-5" />
                 </div>
             ) : (
-                <div>
-                    <Table>
-                        <TableHeader>
-                            <TableRow className="hover:bg-transparent border-none">
-                                <TableHead>
-                                    Name
-                                </TableHead>
-                                <TableHead>
-                                    &nbsp;
-                                </TableHead>
-                                <TableHead className="hidden md:table-cell">
-                                    Shared
-                                </TableHead>
-                                <TableHead className="hidden md:table-cell">
-                                    Created at
-                                </TableHead>
+                <Table>
+                    <TableHeader>
+                        <TableRow className="hover:bg-transparent border-none">
+                            <TableHead>
+                                Name
+                            </TableHead>
+                            <TableHead>
+                                &nbsp;
+                            </TableHead>
+                            <TableHead className="hidden md:table-cell">
+                                Shared
+                            </TableHead>
+                            <TableHead className="hidden md:table-cell">
+                                Created at
+                            </TableHead>
 
+                        </TableRow>
+                    </TableHeader>
+                    {documents.length === 0 ? (
+                        <TableBody>
+                            <TableRow className="hover:bg-transparent">
+                                <TableCell colSpan={4} className="h-24 text-center text-muted-foreground">
+                                    No documents found
+                                </TableCell>
                             </TableRow>
-                        </TableHeader>
-                        {documents.length === 0 ? (
-                            <TableBody>
-                                <TableRow className="hover:bg-transparent">
-                                    <TableCell colSpan={4} className="h-24 text-center text-muted-foreground">
-                                        No documents found
-                                    </TableCell>
-                                </TableRow>
-                            </TableBody>
-                        ) : (
-                            <TableBody>
-                                {
-                                    documents.map((document) => (
-                                        <DocumentRow key={document._id}
-                                            document={document}
-                                        />
-                                    ))
-                                }
-                            </TableBody>
-
-                        )}
-                    </Table>
-                </div>
+                        </TableBody>
+                    ) : (
+                        <TableBody>
+                            {
+                                documents.map((document) => (
+                                    <DocumentRow key={document._id}
+                                        document={document}
+                                    />
+                                ))
+                            }
+                        </TableBody>
+                    )}
+                </Table>
             )}
+            <div className="flex items-center justify-center">
+                <Button
+                    variant="ghost"
+                    size="sm"
+                    onClick={() => loadMore(5)}
+                    disabled={status !== "CanLoadMore"}
+                >
+                    {status === "CanLoadMore" ? "Load more" : "End of results"}
+                </Button>
+            </div>
         </div>
     )
 }
