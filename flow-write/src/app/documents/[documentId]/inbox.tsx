10cb0d611b0dd74a36c68bb16ce99ac0a2656f46
Sankeerthan <sankeerthandayanidi@hotmail.com>
2024-12-14 14:38:38 +0530

    inbox menu logic works

--- flow-write/src/app/documents/[documentId]/inbox.tsx
+++ flow-write/src/app/documents/[documentId]/inbox.tsx
@@ -1,7 +1,10 @@
 "use client"
 import { BellIcon } from "lucide-react"
 import { InboxNotification, InboxNotificationList } from "@liveblocks/react-ui"
+import { useInboxNotifications } from "@liveblocks/react"
 import { ClientSideSuspense } from "@liveblocks/react"
+import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
 
 export const Inbox = () => {
     return (
@@ -12,7 +15,21 @@ export const Inbox = () => {
 }
 
 const InboxMenu = () => {
-    return (
 
+    const { inboxNotifications } = useInboxNotifications();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button
+                    variant={"ghost"}
+                    className="relative"
+                    size="icon"
+                >
+                    <BellIcon className="size-5" />
+                </Button>
+            </DropdownMenuTrigger>
+
+        </DropdownMenu>
     );
 }
